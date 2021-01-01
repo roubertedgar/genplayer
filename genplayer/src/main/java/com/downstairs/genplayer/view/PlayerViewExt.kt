bc93cc918a3b91320027d3c3f05682e75da286a2
Roubert Edgar <roubertedgar@gmail.com>
2021-01-01 00:01:24 -0300

    Reformat player view ext

--- genplayer/src/main/java/com/downstairs/genplayer/view/PlayerViewExt.kt
+++ genplayer/src/main/java/com/downstairs/genplayer/view/PlayerViewExt.kt
@@ -3,7 +3,6 @@ package com.downstairs.genplayer.view
 import com.downstairs.genplayer.engine.PlayerEngine
 import com.google.android.exoplayer2.ui.PlayerView
 
-
 fun PlayerView.setPlayer(playerEngine: PlayerEngine) {
     this.player = playerEngine.player
 }
