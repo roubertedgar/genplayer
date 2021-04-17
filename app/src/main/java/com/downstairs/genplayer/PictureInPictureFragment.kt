5f3cf375182c42c310423a77f8e760cd13f313d4
Roubert Edgar <roubertedgar@gmail.com>
2021-04-17 19:38:50 -0300

    Removes unused codes

--- app/src/main/java/com/downstairs/genplayer/PictureInPictureFragment.kt
+++ app/src/main/java/com/downstairs/genplayer/PictureInPictureFragment.kt
@@ -1,9 +1,5 @@
 package com.downstairs.genplayer
 
-import android.util.TypedValue
-import androidx.annotation.AttrRes
-import androidx.fragment.app.Fragment
-
 interface PictureInPictureFragment {
 
     val wishEnterOnPipMode: Boolean
@@ -11,18 +7,4 @@ interface PictureInPictureFragment {
     fun enterOnPictureInPicture()
 
     fun exitFromPictureInPicture()
-}
-
-fun Fragment.getThemeColor(@AttrRes colorRes: Int): Int {
-    var color = 0
-
-    context?.also {
-        TypedValue().run {
-            val colors = it.obtainStyledAttributes(data, intArrayOf(colorRes))
-            color = colors.getColor(0, 0)
-            colors.recycle()
-        }
-    }
-
-    return color
 }
